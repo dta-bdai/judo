103a2640b2cffd2be36057be99478683442345a9
John Zhang <jzhang@theaiinstitute.com>
2025-12-04 11:31:07 -0500

    add save json

--- scripts/benchmark_sampling.py
+++ scripts/benchmark_sampling.py
@@ -1,6 +1,6 @@
 from performance_benchmark import benchmark_multiple_tasks_and_optimizers
 
-def benchmark_sampling_sumo(num_episodes: int = 50):
+def benchmark_sampling_sumo(num_episodes: int = 50, save_results: bool = False):
     optimizer_names = [
         "cem",
     ]
@@ -16,28 +16,28 @@ def benchmark_sampling_sumo(num_episodes: int = 50):
         num_episodes=num_episodes,
         episode_length_s=30.0,
         viz_dt=0.02,
-        save_results=False,
+        save_results=save_results,
     )
 
-def benchmark_sampling_baseline(num_episodes: int = 50):
+def benchmark_sampling_baseline(num_episodes: int = 50, save_results: bool = False):
     optimizer_names = [
         "cem",
     ]
     benchmark_multiple_tasks_and_optimizers(
         task_names=[
             "spot_box_push_baseline",
-            "spot_chair_push_baseline",
-            "spot_tire_push_baseline",
-            "spot_rack_push_baseline",
-            "spot_cone_push_baseline",
+            # "spot_chair_push_baseline",
+            # "spot_tire_push_baseline",
+            # "spot_rack_push_baseline",
+            # "spot_cone_push_baseline",
         ],
         optimizer_names=optimizer_names,
         num_episodes=num_episodes,
         episode_length_s=30.0,
         viz_dt=0.02,
-        save_results=False,
+        save_results=save_results,
     )
 
 if __name__ == "__main__":
     # benchmark_sampling_sumo(num_episodes=50)
-    benchmark_sampling_baseline(num_episodes=10)
+    benchmark_sampling_baseline(num_episodes=1, save_results=True)
